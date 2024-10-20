1320759e31ee17dce07996abaa9f74cc28ef04b6
James Carpenter <james@james-carpenter.net>
2024-10-20 15:45:08 +1100

    chore: add state class for yield sensors, for energy page

--- custom_components/solax_cloud/sensor.py
+++ custom_components/solax_cloud/sensor.py
@@ -144,6 +144,7 @@ SENSOR_TYPES = [
         translation_key="yield_today",
         device_class=SensorDeviceClass.ENERGY,
         native_unit_of_measurement="kWh",
+        state_class="total_increasing",
     ),
     SensorEntityDescription(
         key="yieldtotal",
@@ -151,6 +152,7 @@ SENSOR_TYPES = [
         translation_key="yield_total",
         device_class=SensorDeviceClass.ENERGY,
         native_unit_of_measurement="kWh",
+        state_class="total_increasing",
     ),
     SensorEntityDescription(
         key="feedinpower",
